28fadd8485e2af2875d0ca804d429eb6f782324a
Chris <chrisbreuer93@gmail.com>
2025-02-01 14:09:14 +0100

    chore: wip
    chore: wip
    
    chore: wip

---
+++ src/types.ts
@@ -0,0 +1,17 @@
+import type * as colors from './colors'
+
+export interface ClarityConfig {
+  verbose: boolean
+}
+
+export type ClarityOptions = Partial<ClarityConfig>
+
+export type LogLevel = 'debug' | 'info' | 'success' | 'warning' | 'error'
+
+export type LogColors = keyof typeof colors
+
+export interface LogEntry {
+  timestamp: Date
+  level: LogLevel
+  message: any
+}
