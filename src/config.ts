fc3bac18690f123192e2d58ac71109a8ab5f9d52
Chris <chrisbreuer93@gmail.com>
2025-02-25 14:25:02 -0800

    chore: wip

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import type { ClarityConfig } from './types'
 import { homedir } from 'node:os'
 import { join } from 'node:path'
 import process from 'node:process'
-import { loadConfig as bunfigLoadConfig } from 'bunfig'
+import { loadConfig } from 'bunfig'
 
 // Default log directory is in user's home directory
 const defaultLogDirectory = process.env.CLARITY_LOG_DIR || join(homedir(), '.clarity', 'logs')
@@ -28,31 +28,6 @@ export const defaultConfig: ClarityConfig = {
     encrypt: false,
   },
   verbose: false,
-}
-
-// Wrapper around bunfig's loadConfig that handles verbose mode
-async function loadConfig() {
-  const isVerbose = process.env.CLARITY_VERBOSE === 'true' || defaultConfig.verbose
-
-  // Temporarily override console.error if not in verbose mode
-  const originalError = console.error
-  if (!isVerbose) {
-    console.error = () => {}
-  }
-
-  try {
-    return await bunfigLoadConfig({
-      name: 'clarity',
-      defaultConfig,
-      cwd: process.cwd(),
-      endpoint: '',
-      headers: {},
-    })
-  }
-  finally {
-    // Always restore console.error
-    console.error = originalError
-  }
 }
 
 // @ts-expect-error there is a current dtsx issue
