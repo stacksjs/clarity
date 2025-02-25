bc87ece62854d75e9ee6e0f0dcc0516119e25400
Chris <chrisbreuer93@gmail.com>
2025-02-24 18:16:49 -0800

    chore: several minor updates
    chore: wip

--- test/browser.test.ts
+++ test/browser.test.ts
@@ -1,1 +1,7 @@
-// wip
+import { expect, test } from 'bun:test'
+
+test('set button text', () => {
+  document.body.innerHTML = `<button>My button</button>`
+  const button = document.querySelector('button')
+  expect(button?.innerText).toEqual('My button')
+})
