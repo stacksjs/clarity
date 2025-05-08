72b1f694703bdb2ddb6e8c98f01a04a04b011c40
cab-mikee <mike.cabz32@gmail.com>
2025-05-08 18:56:43 +0800

    chore: fix CLI log directory error

--- build.ts
+++ build.ts
@@ -1,17 +1,9 @@
 import { dts } from 'bun-plugin-dtsx'
 
 await Bun.build({
-  entrypoints: ['src/index.ts'],
+  entrypoints: ['src/index.ts', 'bin/cli.ts'],
   outdir: './dist',
   target: 'bun',
-  plugins: [dts()],
-})
-
-// Build the CLI
-await Bun.build({
-  entrypoints: ['bin/cli.ts'],
-  target: 'bun',
-  outdir: './dist/bin',
   plugins: [dts()],
 })
 
